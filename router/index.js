0d666971ff021090e0e2c6ac84c88c28db09b898
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-06-11 17:33:37 -0400

    Add EditPost view and /post/:id/edit route

--- router/index.js
+++ router/index.js
@@ -4,6 +4,7 @@ import PostList from '../views/PostList.vue';
 import PostContent from '../views/PostContent.vue';
 import UserAuth from '../views/UserAuth.vue';
 import NewPost from '../views/NewPost.vue';
+import EditPost from '../views/EditPost.vue';
 
 
 const router = createRouter({
@@ -12,6 +13,7 @@ const router = createRouter({
     { path: '/', redirect: '/posts' },
     { path: '/posts', component: PostList, },
     { path: '/posts/:id', component: PostContent, props: true },
+    { path: '/posts/:id/edit', component: EditPost, props: true },
     { path: '/auth', component: UserAuth },
     { path: '/newPost', component: NewPost },
   ],
---
+++ views/EditPost.vue
@@ -0,0 +1,3 @@
+<template>
+  EditPost
+</template>
