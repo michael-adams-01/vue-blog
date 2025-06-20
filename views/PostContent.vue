2f6afae88d90cfaffced883b4b3b23bb5088231f
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-06-20 15:30:17 -0500

    Fix bug in PostContent where page broke on reload

--- views/PostContent.vue
+++ views/PostContent.vue
@@ -3,7 +3,6 @@
     <h1 class="text-3xl mt-3 font-bold">{{ postData.title }}</h1>
     <h2 class="text-sm mt-3">{{ postData.datePublished }}</h2>
     <p class="mt-4 text-3xl"> {{ postData.postText }}</p>
-    <!-- <h3 class="mt-3">The post id is: {{ route.params.id }}</h3> -->
   </base-card>
 </template>
 
@@ -16,15 +15,11 @@ const store = useBlogStore();
 
 
 const route = useRoute();
-//console.log(route.params.id)
-//console.log(store.blogPosts)
+
+store.fetchPosts()
 
 const postData = computed(function () {
   return store.blogPosts.find(post => post.id === route.params.id);
 });
-
-console.log(postData.value.title)
-
-
 
 </script>
