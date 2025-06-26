5a80a03e5e137d1da8b42e902496f0f5e1f11ef2
michael <87049005+michael-adams-01@users.noreply.github.com>
2025-06-26 12:05:28 -0500

    Add Edit feature allowing admin to edit blog posts.

--- views/EditPost.vue
+++ views/EditPost.vue
@@ -1,3 +1,87 @@
 <template>
-  EditPost
+  <div>
+    <h1 class="mt-3 text-center text-3xl mb-5">EDIT PAGE</h1>
+    <div v-if="postData" class="flex flex-col">
+      <input @focus="titleIsInvalid = false" v-model.trim="postTitle" class="font-bold mx-auto border rounded-lg p-2"
+        type="text">
+      <h3 class="text-center font-bold m-3 text-red-500" v-if="titleIsInvalid">Please enter a title</h3>
+      <textarea @focus="textIsInvalid = false" v-model.trim="postText" class="mt-5 mx-auto border rounded-lg p-2"
+        id="blogText" name="blogText" cols="50" rows="15"></textarea>
+      <h3 class="text-center font-bold m-3 text-red-500" v-if="textIsInvalid">Please enter blog text</h3>
+      <button @click="submitData" class="mx-auto font-bold bg-blue-500 text-white p-2 rounded-lg mt-3 hover:bg-blue-400
+      hover:scale-96">Submit</button>
+    </div>
+  </div>
 </template>
+
+<script>
+import { useBlogStore } from "../stores/blog.js"
+export default {
+  setup() {
+    const store = useBlogStore();
+    return { store }
+  },
+  data() {
+    return {
+      postTitle: '',
+      postText: '',
+      titleIsInvalid: false,
+      textIsInvalid: false,
+    }
+  },
+  methods: {
+    async getData() {
+      await this.store.fetchPosts();
+      this.postTitle = this.postData.title
+      this.postText = this.postData.postText
+    },
+    validateForm() {
+      if (this.postTitle === '') {
+        console.log('postTitle is empty')
+        this.titleIsInvalid = true;
+      }
+      if (this.postText === '') {
+        console.log('postText is empty')
+        this.textIsInvalid = true;
+      }
+    },
+    async submitData() {
+      //validate form data
+      this.validateForm();
+      console.log(this.formIsValid)
+
+      if (this.titleIsInvalid || this.textIsInvalid) {
+        return;
+      }
+
+      const response = await
+        fetch(`https://vue-blog-68392-default-rtdb.firebaseio.com/posts/${this.postData.id}.json`, {
+          method: 'PUT',
+          body: JSON.stringify({
+            title: this.postTitle,
+            postText: this.postText
+          })
+        })
+
+      const responseData = await response.json();
+
+      if (!response.ok) {
+        const error = new Error(response.error.message || 'There was an error submitting your post')
+        throw error;
+      }
+
+
+      await this.store.updateData(this.postTitle, this.postText)
+      this.$router.replace(`/posts/${this.$route.params.id}/`)
+    },
+  },
+  computed: {
+    postData() {
+      return this.store.blogPosts.find(post => post.id === this.$route.params.id);
+    }
+  },
+  mounted() {
+    this.getData()
+  }
+}
+</script>
--- views/PostContent.vue
+++ views/PostContent.vue
@@ -1,9 +1,14 @@
 <template>
-  <base-card>
-    <h1 class="text-3xl mt-3 font-bold">{{ postData.title }}</h1>
-    <h2 class="text-sm mt-3">{{ postData.datePublished }}</h2>
-    <p class="mt-4 text-3xl"> {{ postData.postText }}</p>
-  </base-card>
+  <div>
+    <h1 v-if="!postData">Error loading page. Please try reloading.</h1>
+    <base-card class="flex flex-col" v-else>
+      <h1 class="text-3xl mt-3 font-bold">{{ postData.title }}</h1>
+      <h2 class="text-sm mt-3">{{ postData.datePublished }}</h2>
+      <p class="mt-4 text-3xl"> {{ postData.postText }}</p>
+      <router-link v-if="store.isLoggedIn" class="mx-auto font-bold bg-green-500 text-white p-2 rounded-lg mt-3
+        hover:bg-green-400 hover:scale-96" :to="`/posts/${postData.id}/edit`">Edit</router-link>
+    </base-card>
+  </div>
 </template>
 
 <script setup>
@@ -16,7 +21,13 @@ const store = useBlogStore();
 
 const route = useRoute();
 
+async function getPosts() {
+  await store.fetchPosts()
+}
+getPosts()
+
 store.fetchPosts()
+
 
 const postData = computed(function () {
   return store.blogPosts.find(post => post.id === route.params.id);
